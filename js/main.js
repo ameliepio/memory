1d56bd6e5f5fa17601e312e9cfb4867bde0c7d54
amelie59780 <a.pioger@outlook.fr>
2017-07-19 11:02:37 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+// les variables
+var tablimages= document.getElementsByTagName('figure');//déclaration tableau d'images*
+console.log (tablimages);
+var tabljeu =document.getElementsByTagName('figure');//déclaration tableau d'images*;//déclaration des cartes, tableau d'images x2 mélangées
+var bob=document.getElementsByClassName('imageface1');//déclaration tableau d'images*
+var other=document.getElementsByClassName('imageface2');//déclaration tableau d'images*//déclaration image de dos
+// var affectcarte=
+var paire=0;//compte les paires
+var temp=2000;;//base du temps
+var nbrecoup=0;//nombre de click
+var redistrib= "";//déclaration enrgistrement addresse pour redistribution
+
+
+// les fonctions
+
+function melange{}
+
+function redistrib{}
+
+function opacity{}
+
+function testimages{}
